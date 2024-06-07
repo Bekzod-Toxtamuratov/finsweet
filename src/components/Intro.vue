96bd4a05664ee9305ac8bf226599bbbd34838fde
Bekzod <bekzodtoxtamuratov@gmail.com>
2024-06-07 16:31:45 +0300

    ok

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,17 +2,15 @@
 	<div>
 		<section class="bg-[#1C1E53] py-[128px]">
 			<div class="container">
-				<div
-					class="max-md:border max-md:flex-col border-red-500 md:flex gap-x-8"
-				>
+				<div class="max-md:border max-md:flex-col md:flex gap-x-8">
 					<div class="">
 						<h1
-							class="mb-7 text-[25px] md:text-[54px] max-w-[350px] max-md:text-center  mx-auto md:max-w-[582px] text-[white] font-bold leading-[42px] md:leading-[74px]"
+							class="mb-7 text-[25px] md:text-[54px] max-w-[350px] max-md:text-center mx-auto md:max-w-[582px] text-[white] font-bold leading-[42px] md:leading-[74px]"
 						>
 							Building stellar websites for early startups
 						</h1>
 						<p
-							class="max-md:max-w-[380px]  max-md:text-center  max-md:mx-auto max-w-[460px] font-medium text-[16px] leading-7 text-[white]"
+							class="max-md:max-w-[380px] max-md:text-center max-md:mx-auto max-w-[460px] font-medium text-[16px] leading-7 text-[white]"
 						>
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 							eiusmod tempor incididunt.
@@ -21,7 +19,7 @@
 							class="max-md:mb-[50px] flex gap-x-[55px] mt-[50px] items-center max-md:justify-center"
 						>
 							<button
-								class="max-md:ml-4 rounded-3xl font-medium border border-white-500 py-3 px-8 md:py-4 md:px-16 bg-[#FCD980]"
+								class="max-md:ml-4 rounded-3xl sm:font-medium border border-white-500  px-4 py-2  sm:py-3 sm:px-8 md:py-4 md:px-16 bg-[#FCD980]"
 							>
 								View our work
 							</button>
@@ -35,7 +33,10 @@
 						</div>
 					</div>
 					<div class="flex">
-						<img class="max-md:w-[60%] max-md:mx-auto" src="/images/intro.png" />
+						<img
+							class="max-md:w-[60%] max-md:mx-auto"
+							src="/images/intro.png"
+						/>
 					</div>
 				</div>
 			</div>
